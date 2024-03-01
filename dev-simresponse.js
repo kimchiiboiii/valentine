d7b395b8b802474f1f1ce9932d05befd5169f280
kimchiiboiii <matt.hernz@gmail.com>
2024-03-01 16:04:37 -0500

    trying to figure out how to merge a different branch correctly

---
+++ dev-simresponse.js
@@ -0,0 +1,47 @@
+
+
+let clickTotal = 0;
+
+
+clicker.addEventListener(addClick);
+
+
+
+
+
+function addClick() {
+
+    clickTotal += 1;
+    let heart = document.getElementById("progressHeart1");
+    let heart2 = document.getElementById("progressHeart2");
+    let scoreTotal = document.getElementById("scoreKeep");
+    scoreTotal.innerHTML = "Score: " + clickTotal;
+    
+    if (clickTotal >= 10) {
+
+        heart.style.opacity = "0.3"
+        heart2.style.opacity = "0.3"
+
+
+    }
+    if (clickTotal >= 20) {
+        heart.style.opacity = "0.5"
+        heart2.style.opacity = "0.5"
+    }
+    if (clickTotal >= 30) {
+        heart.style.opacity = "0.8"
+        heart2.style.opacity = "0.8"
+    }
+    if (clickTotal >= 40) {
+        heart.style.opacity = "1"
+        heart2.style.opacity = "1"
+    }
+      
+
+    
+
+    
+
+    
+}
+
